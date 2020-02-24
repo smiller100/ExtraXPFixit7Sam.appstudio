558c13866faf598b03c9d564e6e59a4021c61a8f
smiller100 <srm08530@creighton.edu>
2020-02-23 20:46:27 -0600

    final update

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,7 @@
+/*
+let bestPetDog = "Dogs are likely the most stereotypical pet for children – and there’s a reason for that. Most average-sized dogs are large enough to avoid being injured by your child, but many breeds are also calm and laidback with children. Dogs easily live to a decade, so you shouldn’t have to worry about having that uncomfortable conversation with your child. This species is also trainable and can usually go anywhere your child can, so they make good everyday companions."
+
+//Use the string method 'replace' with the bestPetDog variable to replace all of the 'dog' strings in the text with 'hamster'.
+let replacedBestPetDog = bestPetDog.replace("Dog","Hamster")
+alert(replacedBestPetDog)
+*/
